e1b9dbbc87099ffa8e188357919e7d150413439d
yuvwwa <150217655+yuvwwa@users.noreply.github.com>
2025-06-03 00:40:23 +0800

    Delete indexes.sql

--- indexes.sql
+++
@@ -1,7 +0,0 @@
-create index idx_products_category on products(category_id);
-create index idx_products_active on products(is_active);
-create index idx_orders_date on orders(order_date);
-create index idx_orders_customer on orders(customer_id);
-create index idx_orders_status on orders(status_id);
-create index idx_customers_phone on customers(phone);
-create index idx_ingredients_stock on ingredients(current_stock);
